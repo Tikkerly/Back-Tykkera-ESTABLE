8535a454462111e9a9641e8523482cddd0cfe9b1
Pilar Baroni <122078706+PilarBaroni@users.noreply.github.com>
2023-11-07 16:41:05 -0300

    deleteUser

--- src/controllers/userControllers/index.js
+++ src/controllers/userControllers/index.js
@@ -1,11 +1,13 @@
 const loginUser = require('./loginUser/index');
 const registerUser = require('./registerUser/index');
-const editUser = require('./editUser/index')
-const getUsers = require('./getUsers/index')
+const editUser = require('./editUser/index');
+const getUsers = require('./getUsers/index');
+const deleteUser = require('./deleteUser/index');
 
 module.exports = {
     loginUser,
     registerUser,
     editUser,
-    getUsers
+    getUsers,
+    deleteUser
 }
