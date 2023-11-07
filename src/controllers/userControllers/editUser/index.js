0de41237bb8380e6f23cc203517ae491a7d0ccef
MDKMauricioKlainbard <mauricioklainbard01@gmail.com>
2023-11-07 15:59:06 -0300

    edit-user-controller

--- src/controllers/userControllers/editUser/index.js
+++ src/controllers/userControllers/editUser/index.js
@@ -1,8 +1,17 @@
-const editUser = async (req,res) => {
+const { hashPassword } = require('../../../helpers/index')
+const User = require('../../../models/User')
+
+const editUser = async (req, res) => {
     try {
-        
+        const { id } = req.params;
+        const { _id, password, google, email, ...rest } = req.body;
+        if (password) {
+            resto.password = hashPassword(password)
+        }
+        const user = await User.findByIdAndUpdate(id, rest);
+        return res.status(200).json(user);
     } catch (error) {
-        
+        return res.status(400).json({ message: error.message })
     }
 }
 
