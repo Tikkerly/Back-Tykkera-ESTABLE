b0ab6ee27b3b8f3b3991a08ca0f11c60a97bc911
MDKMauricioKlainbard <mauricioklainbard01@gmail.com>
2023-11-07 16:06:44 -0300

    get-users

--- src/controllers/userControllers/index.js
+++ src/controllers/userControllers/index.js
@@ -1,9 +1,11 @@
 const loginUser = require('./loginUser/index');
 const registerUser = require('./registerUser/index');
 const editUser = require('./editUser/index')
+const getUsers = require('./getUsers/index')
 
 module.exports = {
     loginUser,
     registerUser,
-    editUser
+    editUser,
+    getUsers
 }
