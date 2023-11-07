ce9ace4acd1bb3ba9a4957408edd060649fce51a
MDKMauricioKlainbard <mauricioklainbard01@gmail.com>
2023-11-07 16:16:03 -0300

    connection_DB

--- src/database/config.js
+++ src/database/config.js
@@ -1,13 +1,10 @@
+const mongoose = require('mongoose');
+
 const dbConnection = async () => {
 
   try {
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
+    await mongoose.connect(process.env.MONGO_URI, {});
 
     console.log('Base de datos online');
 
