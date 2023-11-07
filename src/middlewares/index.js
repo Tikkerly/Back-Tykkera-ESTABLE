74ec82eb3051960e9081576cda35f3e8c6607637
Joel Lopez <djoel95@hotmail.com>
2023-11-07 19:51:22 -0400

    feature/protectAdminRoutes

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,9 @@
 // const checkJWT = require('./auth/index')
 const { fieldsValidate } = require("./validateErrors/index");
 const { validarJWT } = require("./validateTokens/index");
+const { adminRole } = require("./protectAdmin/index");
 module.exports = {
   fieldsValidate,
   validarJWT,
+  adminRole,
 };
