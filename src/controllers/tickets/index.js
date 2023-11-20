b1d28b935c515b9f379824e42153bd3d2afd50fa
Sebastian Gonzalez Conosciuto <conosciutosebastian@gmail.com>
2023-11-20 16:27:01 -0300

    getTicketById

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,11 +1,12 @@
 const deleteTicket = require("./delete/index");
 const updateTicket = require("./update/index");
 const registerTicket = require("./register/index");
-const { getTicketsByAgent, getTicketsByUser } = require("./get/index");
+const { getTicketsByAgent, getTicketsByUser, getTicketById } = require("./get/index");
 module.exports = {
   deleteTicket,
   updateTicket,
   registerTicket,
   getTicketsByAgent,
   getTicketsByUser,
+  getTicketById
 };
