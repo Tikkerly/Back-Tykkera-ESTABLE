07630aa12a77e4868a22cf2a40250c850835baa6
Hugo Tamayo <doctamayot@hotmail.com>
2023-11-20 20:48:03 -0500

    Back pre demo2

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.use(cors(corsOptions));
+server.use(cors(corsOptions));
 
 server.use(express.json());
 //server.use(cors());
