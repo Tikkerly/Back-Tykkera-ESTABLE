2d516019b41504f29eca42f760c9e58ab6e0ca90
Hugo Tamayo <doctamayot@hotmail.com>
2023-11-20 21:31:51 -0500

    Back pre demo2

--- src/server.js
+++ src/server.js
@@ -7,21 +7,8 @@ require("dotenv").config();
 
 const server = express();
 
-const allowedOrigins = ["http://localhost:3000"]; // Reemplaza con la URL de tu aplicación Next.js
-
-const corsOptions = {
-  origin: ["https://tu-aplicacion-next.vercel.app", "http://localhost:3000"], // Permitir solicitudes desde tu aplicación Next.js y desde localhost
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-server.use(cors(corsOptions));
-
-server.use(cors(corsOptions));
-
 server.use(express.json());
-//server.use(cors());
+server.use(cors());
 server.use(morgan("dev"));
 server.use(
   fileUpload({
